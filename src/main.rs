9c8a1aabe1678d9dad6a827d6a598a63d84cce73
Chris Westra <cwestra@gmail.com>
2018-07-09 12:57:22 -0400

    WIP chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
 extern crate lox;
+extern crate rprompt;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::env;
 
 use lox::chunk::{Chunk, OpCode};
 use lox::vm::{VM};
 fn main() {
-    let mut c = Chunk::new();
-    c.write(OpCode::Constant, 123);
-    let b = c.add_constant(1.2);
-    c.write(b, 123);
-
-    let constant = c.add_constant(3.4);
-    c.write(OpCode::Constant, 123);
-    c.write(constant, 123);
-
-    c.write(OpCode::Add, 123);
-
-    let constant = c.add_constant(5.6);
-    c.write(OpCode::Constant, 123);
-    c.write(constant, 123);
-
-    c.write(OpCode::Divide, 123);
-    c.write(OpCode::Negate, 123);
-
-    c.write(OpCode::Return, 123);
-    c.disassemble_with_iterator("test chunk");
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(file_path) => interpret_file(file_path),
+        None => repl(),
+    }
 
     let mut vm = VM::new(&c);
     println!("Running");
     vm.run().unwrap();
+}
+
+
+fn repl() {
+    println!("Lox Repl");
+    loop {
+        let input = rprompt::prompt_reply_stdout(">").unwrap();
+    }
+}
+
+fn interpret_file(path: &str) {
+    let file = File::open(path).unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+    
 }
