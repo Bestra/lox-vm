6a101cdd157eeb4cd2164584c0b29125215567bf
Chris Westra <cwestra@gmail.com>
2018-07-10 21:02:03 -0400

    WIP scanning

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,52 +1,54 @@
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
 enum TokenType {
     // Single-character tokens.
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
-    COMMA,
-    DOT,
-    MINUS,
-    PLUS,
-    SEMICOLON,
-    SLASH,
-    STAR,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
 
     // One or two character tokens.
-    BANG,
-    BANG_EQUAL,
-    EQUAL,
-    EQUAL_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    LESS,
-    LESS_EQUAL,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
     // Literals.
-    IDENTIFIER,
-    STRING,
-    NUMBER,
+    Identifier,
+    String,
+    Number,
 
     // Keywords.
-    AND,
-    CLASS,
-    ELSE,
-    FALSE,
-    FUN,
-    FOR,
-    IF,
-    NIL,
-    OR,
-    PRINT,
-    RETURN,
-    SUPER,
-    THIS,
-    TRUE,
-    VAR,
-    WHILE,
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
 
-    ERROR,
-    EOF,
+    Error,
+    Eof,
 }
 
 struct Token<'t> {
@@ -54,7 +56,7 @@ struct Token<'t> {
     length: usize,
     line: usize,
     t: TokenType,
-    source: &'t str, 
+    source: &'t str,
 }
 struct Scanner<'a> {
     start: usize,
@@ -63,7 +65,7 @@ struct Scanner<'a> {
     source: &'a str,
 }
 
-impl<'a> Scanner<'a> {
+impl Scanner<'a> {
     pub fn new(source: &'a str) -> Scanner {
         Scanner {
             start: 0,
@@ -77,18 +79,105 @@ impl<'a> Scanner<'a> {
         self.current
     }
 
-    pub fn is_at_end(&mut self) -> bool {
-      self.current >= self.source.len()
+    // pub fn is_at_end_mut(&mut self) -> bool {
+    //     self.current >= self.source.len()
+    // }
+
+    pub fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
     }
 
     pub fn scan_token(&mut self) -> Token {
+        self.skip_whitespace();
         self.start = self.current;
         if self.is_at_end() {
-            self.make_token(TokenType::EOF)
-        } else {
-            self.error_token("Unexpected character.")
+            return self.make_token(TokenType::Eof);
+        }
+
+        match self.advance() {
+            '(' => self.make_token(TokenType::LeftParen),
+            ')' => self.make_token(TokenType::RightParen),
+            '{' => self.make_token(TokenType::LeftBrace),
+            '}' => self.make_token(TokenType::RightBrace),
+            ';' => self.make_token(TokenType::Semicolon),
+            ',' => self.make_token(TokenType::Comma),
+            '.' => self.make_token(TokenType::Dot),
+            '-' => self.make_token(TokenType::Minus),
+            '+' => self.make_token(TokenType::Plus),
+            '/' => self.make_token(TokenType::Slash),
+            '*' => self.make_token(TokenType::Star),
+            '!' => {
+                if self.consume_match('=') {
+                    self.make_token(TokenType::BangEqual)
+                } else {
+                    self.make_token(TokenType::Bang)
+                }
+            }
+            '=' => {
+                if self.consume_match('=') {
+                    self.make_token(TokenType::EqualEqual)
+                } else {
+                    self.make_token(TokenType::Equal)
+                }
+            }
+            '<' => {
+                if self.consume_match('=') {
+                    self.make_token(TokenType::LessEqual)
+                } else {
+                    self.make_token(TokenType::Less)
+                }
+            }
+            '>' => {
+                if self.consume_match('=') {
+                    self.make_token(TokenType::GreaterEqual)
+                } else {
+                    self.make_token(TokenType::Greater)
+                }
+            }
+            _ => self.error_token("Unexpected character."),
         }
     }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                ' ' | '\r' | '\t' => {
+                    self.advance();
+                }
+                '\n' => {
+                    self.line += 1;
+                    self.advance();
+                },
+                _ => return,
+            };
+        }
+    }
+
+    pub fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source.chars().nth(self.current - 1).unwrap()
+    }
+
+    pub fn consume_match(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        if self.peek() != expected {
+            return false;
+        }
+
+        self.advance();
+        true
+    }
+
+    pub fn peek(&self) -> char {
+        self.source.chars().nth(self.current).unwrap()
+    }
+
+    // pub fn peek_mut(&mut self) -> char {
+    //     self.source.chars().nth(self.current).unwrap()
+    // }
 
     fn make_token(&mut self, t: TokenType) -> Token {
         Token {
@@ -96,19 +185,18 @@ impl<'a> Scanner<'a> {
             start: self.start,
             length: self.current - self.start,
             line: self.line,
-            source: self.source
+            source: self.source,
         }
     }
 
     fn error_token(&mut self, message: &'static str) -> Token {
         Token {
-            t: TokenType::ERROR,
+            t: TokenType::Error,
             start: 0,
             length: message.len(),
             line: self.line,
             source: message,
         }
-
     }
 }
 
@@ -124,4 +212,41 @@ pub fn compile(source: &str) {
             print!("|");
         }
     }
+}
+
+#[test]
+fn scanner_peek_and_advance() {
+    let mut s = Scanner::new("{}");
+    assert_eq!(s.peek(), '{');
+    assert_eq!(s.advance(), '{');
+    assert_eq!(s.peek(), '}');
+    assert_eq!(s.advance(), '}');
+}
+
+#[test]
+fn scanner_scan_token() {
+    let mut s = Scanner::new("{}");
+    assert_eq!(s.scan_token().t, TokenType::LeftBrace);
+    assert_eq!(s.scan_token().t, TokenType::RightBrace);
+}
+
+#[test]
+fn scanner_skip_whitespace() {
+    let mut s = Scanner::new(" { } ");
+    assert_eq!(s.scan_token().t, TokenType::LeftBrace);
+    assert_eq!(s.scan_token().t, TokenType::RightBrace);
+}
+
+#[test]
+fn scanner_skip_whitespace_newline() {
+    let mut s = Scanner::new(" { \n } ");
+    assert_eq!(s.scan_token().line, 1);
+    assert_eq!(s.scan_token().line, 2);
+}
+
+#[test]
+fn scanner_scan_token_with_equals() {
+    let mut s = Scanner::new("!=<");
+    assert_eq!(s.scan_token().t, TokenType::BangEqual);
+    assert_eq!(s.scan_token().t, TokenType::Less);
 }
